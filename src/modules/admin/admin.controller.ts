77ea414aa9cc45cfa23e13d3c2961d2e0edebd40
Abraham-hanks <ekekhorabraham@gmail.com>
2021-08-21 09:21:52 +0100

    add isAdmin guard to admin profile endpoint and clean codebase

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Param, ParseIntPipe, Post, Put, Query, Request, UseInterceptors } from '@nestjs/common';
+import { Param, ParseIntPipe, Put, Query, UseInterceptors } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiExcludeEndpoint, ApiHideProperty, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { GetUser } from 'src/modules/auth/decorator/user.decorator';
 import { parseQueryObj } from 'src/common/utils/query-parser';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
@@ -10,7 +10,6 @@ import { AdminQueryFiltersDto } from './dto/admin-query-filters.dto';
 import { Role } from '../auth/decorator/role.decorator';
 import { SCOPES } from 'src/common/auth/scopes';
 import { RoleGuard } from '../auth/guards/role.guard';
-import { CreateAdminDto } from './dto/admin.dto';
 import { AuthService } from '../auth/auth.service';
 import { ROLE_NAMES } from '../role/constants';
 import { AuditInterceptor } from 'src/common/interceptor/audit.interceptor';
@@ -42,7 +41,7 @@ export class AdminController {
     return this.adminService.findAll(query);
   }
 
-  // @Role(SCOPES.READ_ADMIN)
+  @Role(SCOPES.IS_ADMIN)
   @Get('profile')
   @UseGuards(JwtAuthGuard, RoleGuard)
   async myProfile(
@@ -76,5 +75,4 @@ export class AdminController {
   ) {
     return this.authService.acDeactivateUser(id, ROLE_NAMES.admin, false);
   }
-
 }
